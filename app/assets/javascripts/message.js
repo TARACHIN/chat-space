0a18a296c3bf5e8571704916aa5c5033abd8116a
TARACHIN <yo.hi.0308@gmail.com>
2019-11-03 19:27:17 +0900

    非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,47 @@
+$(function(){
+  function buildHTML(message) {
+    var html = 
+    `<div class="message">
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+        ${ message.name }
+        </div>
+        <div class="upper-message__date">
+        ${ message.created_at }
+        </div>
+      </div>
+      <div class="lower-message">
+      <p class="lower-message__content">
+      ${ message.content }
+      </p>
+      </div>
+    </div>`
+    return html
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function (data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val("");
+      $('.form__textfield').val('');
+      $('.form__submit').prop('disabled', false);
+      $('.messages').animate({ scrollTop: $(".messages")[0].scrollHeight });
+      return true
+    })
+    .fail(function () {
+      alert('メッセージ送信に失敗しました。');
+    })
+  })
+})
